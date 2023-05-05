d3ca675dab84d77293c52bf8e3f5768b25d11e07
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2023-05-05 12:18:16 +0300

    final commit

--- ELC3252G01.m
+++ ELC3252G01.m
@@ -24,6 +24,9 @@ B8 = tf(K3,1);
 B9 = tf([F2,0],1); 
 B10 = tf(1,[M2,0,0]); 
 B11 = tf(K2,1);
+
+
+% ANTHOR SOLUTION
 % B2 = tf([F1,K1+K2],1)
 % B3 = tf(1,[M1,0,0]); 
 % B4 = tf(K2,1); 
@@ -47,6 +50,7 @@ connect_map =[
     10,6,-7,-8,-9,0; ...
     11,10,0,0,0,0; ...
 ];
+% For sol2
 % connect_map = [
 %     2,3,0,0; ...
 %     3,1,7,-2; ...
@@ -55,21 +59,28 @@ connect_map =[
 %     6,4,-5,0; ...
 %     7,6,0,0; ...
 % ];
+
 input_loc = 1; 
 output_loc = [5,10]; %we get X1 from block 5 and X2 from block 10
+
+% For sol2
 % output_loc = [3,6];
+
+%  CONECT THE BLOCKS
 sys=connect(BlockMat,connect_map,input_loc,output_loc);
+
 % Define a transfer function
 tf_sys = tf(sys);
 
 % Extract the numerator and denominator coefficients
-[num1, den1] = tfdata(tf_sys(1),'v');
-[num2, den2] = tfdata(tf_sys(2),'v');
+[num1, den1] = tfdata(tf_sys(1),'v'); %To get X1/U
+[num2, den2] = tfdata(tf_sys(2),'v'); %To get X2/U
 
 % disp('Numerator Coefficients:');
 % disp(num2);
 % disp('Denominator Coefficients:');
 % disp(den2);  
+
 % we need to check the stability of the system before we can proceed to set the option of RiseTimeLimits
 % because if the system is unstable, we will not proceed to get the T.R
 % if z >1 overdamped system so we won't set the option of RiseTimeLimits but if 0< z <1 underdamped system so we will set the option of RiseTimeLimits
@@ -81,45 +92,37 @@ pzmap(sys);
 % Req 4: If a fixed input force of 1N is applied to the system. Simulate the system under this value of 
 % input force showing the response of X1, X2 also from the resulting responses calculate the steady 
 % state values of these signals. 
-% [wn,z]=damp(sys(1));
 figure(2)
 p = stepplot(sys);
 setoptions(p,'RiseTimeLimits' ,[0,1]);
-% TODO: Compute the steady state values of the signals X1,X2
-
-% disp('Natural Frequency:');
-% disp(wn);
-% disp('Damping Ratio:');
-% disp(z);
-
-
-% Req 5: in report
 
 % Req 6:Simulate the system for a desired level (Xd) of 2 m. showing the response of X2. 
 % Req7 : For the response of X2 calculate the value of 
 % the rise time, peak time, max peak, and settling time. Also calculate the value of ess. 
-H = tf(1,1);
-modif = feedback(tf(num2,den2),H,-1);
+H = tf(1,1); % unity feedback
+modif_sys = feedback(tf(num2,den2),H,-1);
 
 figure(3)
 opt = stepDataOptions('InputOffset',0,'StepAmplitude',2);
-p = stepplot(modif,opt);
+p = stepplot(modif_sys,opt);
 setoptions(p,'RiseTimeLimits' ,[0,1]);
 
-% Req8
+% Req8: As a solution to reduce the value of ess a proportional controller can be used. 
+% Study the effect of the value of proportional controller on both ess and transient response by simulating the system
+% with the following values of P controller: 1, 10,100, and 1000. Calculate transient response parameters for each case. Comment on your results. 
 Kp = 1;
 i = 4;
 j = 5;
 for index = 1:4
     C = tf(Kp,1);
-    modif_p=feedback(C*tf(num2,den2),H,-1);
+    modif_sys_p=feedback(C*tf(num2,den2),H,-1);
     figure(i)
     opt = stepDataOptions('InputOffset',0,'StepAmplitude',2);
-    p = stepplot(modif_p,opt);
+    p = stepplot(modif_sys_p,opt);
     setoptions(p,'RiseTimeLimits' ,[0,1]);
     figure(j)
-    pzmap(modif_p);
-    [wn,z]=damp(modif_p);
+    pzmap(modif_sys_p);
+    [wn,z]=damp(modif_sys_p);
     disp(index);
     disp('Natural Frequency:');
     disp(wn);
@@ -129,17 +132,19 @@ for index = 1:4
     i = i+2;
     j = j+2;
 end
-% REQ 9
+
+% REQ 9: If the desired displacement of the second mass is to be 4 m, 
+% is it possible to obtain a steady state error less than 0.01 m using a proportional-only controller? Why?
 Kp = 4189.5;
 C = tf(Kp,1);
-modif_p=feedback(C*tf(num2,den2),H,-1);
+modif_sys_p=feedback(C*tf(num2,den2),H,-1);
 figure(i)
 opt = stepDataOptions('InputOffset',0,'StepAmplitude',4);
-p = stepplot(modif_p,opt);
+p = stepplot(modif_sys_p,opt);
 setoptions(p,'RiseTimeLimits' ,[0,1]);
 figure(j)
-pzmap(modif_p);
-[wn,z]=damp(modif_p);
+pzmap(modif_sys_p);
+[wn,z]=damp(modif_sys_p);
 disp(index);
 disp('Natural Frequency:');
 disp(wn);
@@ -148,18 +153,19 @@ disp(z);
 
 i = i+2;
 j = j+2;
-% Req 10
+% Req 10:Suggest a suitable controller to eliminate ess. Then, simulate the system using your proposed controller. 
+% Sol: We can use PI controller to get the required ess
 Kp = 100;
 KI = 0.001;
 C = tf([Kp,KI],[1,0]);
-modif_p=feedback(C*tf(num2,den2),H,-1);
+modif_sys_p=feedback(C*tf(num2,den2),H,-1);
 figure(i)
 opt = stepDataOptions('InputOffset',0,'StepAmplitude',4);
-p = stepplot(modif_p,opt);
+p = stepplot(modif_sys_p,opt);
 setoptions(p,'RiseTimeLimits' ,[0,1]);
 figure(j)
-pzmap(modif_p); 
-[wn,z]=damp(modif_p);
+pzmap(modif_sys_p); 
+[wn,z]=damp(modif_sys_p);
 disp(index);
 disp('Natural Frequency:');
 disp(wn);
